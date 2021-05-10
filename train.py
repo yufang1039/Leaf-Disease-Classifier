4e431f050a40c4961d502c7cd2624162de1343e0
Head-inthe-Cloud <cyqppwork@hotmail.com>
2021-05-09 20:30:29 -0700

    Updated train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import math
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -14,26 +15,34 @@ from dataset import LeafDataset
 from sklearn.metrics import accuracy_score
 
 
+
 # Define constant param
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-NUM_OF_EPOCH = 20
+NUM_OF_EPOCH = 5
 LEARNING_RATE = 8e-4
 BATCH_SIZE = 4
 
+# File directories
+label_dir = "train_df_small.csv"
+image_dir = "train_images/"
+save_dir = "results/"
 
 # Get leaf dataset and dataloader for both training and validation dataset
-leaf_ds = LeafDataset(csv_file="train_df.csv", imgs_path="train_images/", 
-                transform=torchvision.transforms.CenterCrop([500, 500]))
+leaf_ds = LeafDataset(csv_file=label_dir, imgs_path=image_dir,
+                transform=torchvision.transforms.Resize([500, 500]))
 ds_len = leaf_ds.__len__()
-train_ds, valid_ds = torch.utils.data.random_split(dataset=leaf_ds, 
-                    lengths=[math.floor(ds_len * 0.7), ds_len - math.floor(ds_len * 0.7)])
-train_loader = DataLoader(train_ds, shuffle=True, batch_size = BATCH_SIZE)
-valid_loader = DataLoader(valid_ds, shuffle=True, batch_size = BATCH_SIZE)
 
 TRAIN_SIZE = math.floor(ds_len * 0.7)
 VALID_SIZE = ds_len - math.floor(ds_len * 0.7)
+
+train_ds, valid_ds = torch.utils.data.random_split(dataset=leaf_ds, 
+                    lengths=[TRAIN_SIZE, VALID_SIZE])
+train_loader = DataLoader(train_ds, shuffle=True, batch_size=BATCH_SIZE)
+valid_loader = DataLoader(valid_ds, shuffle=True, batch_size=BATCH_SIZE)
+
 loss_fn = torch.nn.BCEWithLogitsLoss()
 
+print_running_loss = False
 ## Train funciton that train the model for one epoche
 def train_fn(net, loader):
 
@@ -55,7 +64,8 @@ def train_fn(net, loader):
         tr_loss += loss.item()
         optimizer.step()
 
-        print("One image finished, running loss is" + str(tr_loss/TRAIN_SIZE))
+        if print_running_loss and _ % 10 == 0:
+            print("One image finished, running loss is" + str(tr_loss/TRAIN_SIZE))
 
 
     return tr_loss/TRAIN_SIZE
@@ -89,7 +99,8 @@ valid_loss = []
 train_acc = []
 val_acc = []
 
-if __name__ == "__main__":   
+if __name__ == "__main__":
+    # Training Loop
     for epoch in range(NUM_OF_EPOCH):
 
         os.system(f'echo \"  Epoch {epoch}\"')
@@ -97,13 +108,24 @@ if __name__ == "__main__":
         ## Set mode to training
         leaf_model.train()
         
-        tl = train_fn(leaf_model, loader = train_loader)
-        vl = valid_fn(leeaf_model, loader = valid_loader)
+        tl = train_fn(leaf_model, loader=train_loader)
+        vl = valid_fn(leaf_model, loader=valid_loader)
         train_loss.append(tl)
         valid_loss.append(vl)
-        
 
-        
         print('Epoch: '+ str(epoch) + ', Train loss: ' + str(tl) 
             + ', Val loss: ' + str(vl))
 
+    # Saves model weights, need to specify file name
+    torch.save(leaf_model.state_dict(), save_dir + "weights.pt")
+
+    # Plots Loss
+    epochs = range(1, len(train_loss) + 1)
+    plt.plot(epochs, train_loss, 'y', label = 'Training loss')
+    plt.plot(epochs, valid_loss, 'r', label = 'Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(save_dir + "loss.jpg")
+    plt.show()
